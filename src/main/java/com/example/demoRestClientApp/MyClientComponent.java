c6c4271fa7a07cae81e01fe4a9dcb94c6d31131b
Nilavalagan Sugumaran <>
2020-03-31 12:03:20 +0100

    Added POST

--- src/main/java/com/example/demoRestClientApp/MyClientComponent.java
+++ src/main/java/com/example/demoRestClientApp/MyClientComponent.java
@@ -2,6 +2,9 @@ package com.example.demoRestClientApp;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -24,6 +27,21 @@ public class MyClientComponent {
     public void callService(){
         ResponseEntity<String> response = restTemplate.getForEntity(serviceBaseUrl + 101, String.class);
         log.debug("Response from server, code = {}, body = {} ", response.getStatusCode(), response.getBody());
+
+
+        HttpHeaders header = new HttpHeaders();
+        header.add("Content-Type", "application/xml");
+        HttpEntity<String> entity =
+                new HttpEntity<>(
+                        "<Employee>\n"
+                                + "<name>nila-here</name>\n"
+                                + "<sal>3500.0</sal>\n"
+                                + "</Employee>",
+                        header);
+        response =  restTemplate.exchange(serviceBaseUrl, HttpMethod.POST,entity , String.class);
+
+        log.debug("Response from server, code = {}, body = {} ", response.getStatusCode(), response.getBody());
+
     }
 
     @PostConstruct
