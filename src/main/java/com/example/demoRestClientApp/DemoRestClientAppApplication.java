730bcf13f165e908bbd917528a60d24d99ccb50a
Nilavalagan Sugumaran <>
2020-03-31 11:39:07 +0100

    Further changes

---
+++ src/main/java/com/example/demoRestClientApp/DemoRestClientAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoRestClientApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestClientAppApplication.class, args);
+	}
+
+}
